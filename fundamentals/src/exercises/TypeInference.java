b457618c1b4b6b471d64d1182572d125be2c65b5
gustavomarquezinho <gustavomarquezinho@gmail.com>
2024-02-12 14:24:21 -0300

    Add 'S03L24 - Type Inference'

---
+++ fundamentals/src/exercises/TypeInference.java
@@ -0,0 +1,17 @@
+/*
+ * Title: Type Inference
+ * Section: 03 
+ * Lecture: 24 
+ */
+
+package exercises;
+
+public class TypeInference {
+    public static void main(String[] args) {
+        var number = 5.2;
+        var text = "Hello World";
+
+        System.out.println(number);
+        System.out.println(text);
+    }
+}

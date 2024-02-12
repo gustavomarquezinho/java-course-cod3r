6ad4acda2453a3097ff1fac6463bc11154e124ad
gustavomarquezinho <gustavomarquezinho@gmail.com>
2024-02-12 13:57:24 -0300

    Add 'S03L15 - First Program'

---
+++ fundamentals/src/exercises/FirstProgram.java
@@ -0,0 +1,13 @@
+/*
+ * Title: First Program
+ * Section: 03 
+ * Lecture: 15 
+ */
+
+package exercises;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

614a335ab3bf1dfb9f179d48c9451b5ea401451a
gustavomarquezinho <gustavomarquezinho@gmail.com>
2024-02-12 14:24:32 -0300

    Add 'S03L28 - Import'

---
+++ fundamentals/src/exercises/Import.java
@@ -0,0 +1,19 @@
+/*
+ * Title: Import
+ * Section: 03 
+ * Lecture: 28 
+ */
+
+package exercises;
+
+import java.util.Date;
+
+public class Import {
+    public static void main(String[] args) {
+        java.lang.String hello = "Hello World";
+        System.out.println(hello);
+
+        Date date = new Date();
+        System.out.println(date);
+    }
+}

bf3e629e25db37ce2bba8a3e3ddba9b635ecb5c9
gustavomarquezinho <gustavomarquezinho@gmail.com>
2024-02-12 20:10:43 -0300

    Add overloaded methods

--- oop/src/exercises/Product.java
+++ oop/src/exercises/Product.java
@@ -1,7 +1,7 @@
 /*
  * Title: Product Class
  * Section: 05 
- * Lecture: 93
+ * Lecture: 93, 97
  */
 
 package exercises;
@@ -10,4 +10,12 @@ public class Product {
     String name;
     double price;
     double discount;
+
+    double getFinalPrice() {
+        return this.price * (1 - this.discount);
+    }
+
+    double getFinalPrice(double discountOfManager) {
+        return this.price * (1 - (this.discount + discountOfManager));
+    }
 }
--- oop/src/exercises/ProductTest.java
+++ oop/src/exercises/ProductTest.java
@@ -1,7 +1,7 @@
 /*
  * Title: Product Class Test
  * Section: 05 
- * Lecture: 93
+ * Lecture: 93, 97
  */
 
 package exercises;
@@ -13,23 +13,19 @@ public class ProductTest {
         product1.price = 4567.98;
         product1.discount = 0.05;
 
-        double product1FinalPrice = product1.price * (1 - product1.discount);
-
         System.out.printf("--- %s ---\n", product1.name);
         System.out.printf("Price: $%.2f\n", product1.price);
         System.out.printf("Discount: %.1f%%\n", product1.discount * 100);
-        System.out.printf("Final Price: $%.2f\n\n", product1FinalPrice);
+        System.out.printf("Final Price: $%.2f\n\n", product1.getFinalPrice(0.05));
 
         Product product2 = new Product();
         product2.name = "Pencil";
         product2.price = 1.2;
         product2.discount = 0.01;
 
-        double product2FinalPrice = product2.price * (1 - product2.discount);
-
         System.out.printf("--- %s ---\n", product2.name);
         System.out.printf("Price: $%.2f\n", product2.price);
         System.out.printf("Discount: %.1f%%\n", product2.discount * 100);
-        System.out.printf("Final Price: $%.2f\n\n", product2FinalPrice);
+        System.out.printf("Final Price: $%.2f\n\n", product2.getFinalPrice());
     }
 }

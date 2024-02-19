90a84395d9e5b57677391a41360caeadd3ced27e
gustavomarquezinho <gustavomarquezinho@gmail.com>
2024-02-19 11:34:12 -0300

    Add 'S05L102 - Challenge Data Constructors'

--- oop/src/challenges/ChallengeDate.java
+++ oop/src/challenges/ChallengeDate.java
@@ -1,15 +1,20 @@
  /*
  * Title: Challenge Date
  * 
- * Description (Lecture 94): Create a simple class with just the declaration of 
+ * Description (Lecture 094): Create a simple class with just the declaration of 
  *                           the attributes necessary to compose a date.
  * 
- * Description (Lecture 98): Create a method that will return the formatted date
- *                           (DD/MM/YYYY format). For the string to be reused,
- *                           the method must return the string, not display it.
+ * Description (Lecture 098): Create a method that will return the formatted date
+ *                            (DD/MM/YYYY format). For the string to be reused,
+ *                            the method must return the string, not display it.
+ * 
+ * Description (Lecture 102): Create 2 constructors for the class. The first one
+ *                            will not receive any parameters and the default date
+ *                            will be set 01/01/1970. The second will receive year,
+ *                            month and day as parameters.
  * 
  * Section: 05 
- * Lecture: 94, 98
+ * Lecture: 94, 98, 102
  * 
  * Considerations
  * - Difficulty: Easy
@@ -22,6 +27,18 @@ public class ChallengeDate {
     byte day;
     byte month;
     int year;
+
+    ChallengeDate() {
+        this.year = 1970;
+        this.month = 1;
+        this.day = 1;
+    }
+
+    ChallengeDate(int dateYear, byte dateMonth, byte dateDay) {
+        this.year = dateYear;
+        this.month = dateMonth;
+        this.day = dateDay;
+    }
 
     String getFormattedDate() {
         return String.format("%02d/%02d/%04d", this.day, this.month, this.year);
--- oop/src/challenges/ChallengeDateTest.java
+++ oop/src/challenges/ChallengeDateTest.java
@@ -1,24 +1,19 @@
 /*
  * Title: Challenge Date Class Test
  * Section: 05 
- * Lecture: 94
+ * Lecture: 94, 98, 102
  */
 
 package challenges;
 
 public class ChallengeDateTest {
     public static void main(String[] args) {
-        ChallengeDate date1 = new ChallengeDate();
-        date1.day = 15;
-        date1.month = 1;
-        date1.year = 2311;
-
-        ChallengeDate date2 = new ChallengeDate();
-        date2.day = 1;
-        date2.month = 8;
-        date2.year = 2001;
+        ChallengeDate date1 = new ChallengeDate(2311, (byte) 1, (byte) 15);
+        ChallengeDate date2 = new ChallengeDate(2001, (byte) 8, (byte) 1);
+        ChallengeDate date3 = new ChallengeDate();
 
         System.out.printf("Date 1 -> %s\n", date1.getFormattedDate());
         System.out.printf("Date 2 -> %s\n", date2.getFormattedDate());
+        System.out.printf("Date 3 -> %s\n", date3.getFormattedDate());
     }
 }

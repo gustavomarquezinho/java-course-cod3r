09529d695bc8a9372346d3eebc4781a4126aa17b
gustavomarquezinho <gustavomarquezinho@gmail.com>
2024-02-19 09:14:58 -0300

    Add 'S05L101 - Product Constructors'

--- oop/src/exercises/Product.java
+++ oop/src/exercises/Product.java
@@ -1,7 +1,7 @@
 /*
  * Title: Product Class
  * Section: 05 
- * Lecture: 93, 97
+ * Lecture: 93, 97, 101
  */
 
 package exercises;
@@ -10,6 +10,20 @@ public class Product {
     String name;
     double price;
     double discount;
+
+    Product() {
+    }
+
+    Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    Product(String name, double price, double discount) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+    }
 
     double getFinalPrice() {
         return this.price * (1 - this.discount);
--- oop/src/exercises/ProductTest.java
+++ oop/src/exercises/ProductTest.java
@@ -1,27 +1,21 @@
 /*
  * Title: Product Class Test
  * Section: 05 
- * Lecture: 93, 97
+ * Lecture: 93, 97, 101
  */
 
 package exercises;
 
 public class ProductTest {
     public static void main(String[] args) {
-        Product product1 = new Product();
-        product1.name = "Notebook";
-        product1.price = 4567.98;
-        product1.discount = 0.05;
+        Product product1 = new Product("Notebook", 4567.98, 0.05);
 
         System.out.printf("--- %s ---\n", product1.name);
         System.out.printf("Price: $%.2f\n", product1.price);
         System.out.printf("Discount: %.1f%%\n", product1.discount * 100);
         System.out.printf("Final Price: $%.2f\n\n", product1.getFinalPrice(0.05));
 
-        Product product2 = new Product();
-        product2.name = "Pencil";
-        product2.price = 1.2;
-        product2.discount = 0.01;
+        Product product2 = new Product("Pencil", 1.2);
 
         System.out.printf("--- %s ---\n", product2.name);
         System.out.printf("Price: $%.2f\n", product2.price);

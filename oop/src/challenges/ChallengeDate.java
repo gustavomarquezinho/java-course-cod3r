fe616345a60d7652fbe4822e3efff4721df62b27
gustavomarquezinho <gustavomarquezinho@gmail.com>
2024-02-19 08:34:34 -0300

    Update header comment

--- oop/src/challenges/ChallengeDate.java
+++ oop/src/challenges/ChallengeDate.java
@@ -1,7 +1,15 @@
-/*
- * Title: Challenge Date Class
+ /*
+ * Title: Challenge Date
+ * 
+ * Description: Create a simple class with just the declaration of the attributes
+ *              necessary to compose a date.
+ * 
  * Section: 05 
- * Lecture: 94
+ * Lecture: 94 
+ * 
+ * Considerations
+ * - Difficulty: Easy
+ * - Resolution time: ~1 minute
  */
 
 package challenges;
